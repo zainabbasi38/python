e20d860f9d8da967dd0dfbecf8934e798dfa8bcf
zainabbasi38 <zainabbasi7534@gmail.com>
2025-07-29 14:39:20 +0500

    day-04-code

---
+++ day-05/main.py
@@ -0,0 +1,11 @@
+# THis is a print function + escape sequence.
+"""
+This is multi line
+comment.
+"""
+print("This is day 05 of learning python\nand I am a good boy.")
+print("This is day 05 of learning \"python\"\nand I am a good boy.")
+
+print("hey", 3, 4, sep="&" , end="0003\n" )
+print("Bye")
+
