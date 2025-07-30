9dda33826d35ca145f821b1e37547336a100db85
zainabbasi38 <zainabbasi7534@gmail.com>
2025-07-30 09:59:27 +0500

    data types

---
+++ day-06/README.md
@@ -0,0 +1,4 @@
+#### Variables
+- variables is like a container that store item. 
+- variables store data in RAM memory.
+- variable i a address of specific data.
---
+++ day-06/main.py
@@ -0,0 +1,9 @@
+a = 1
+print(a)
+print("The type of a is ", type(a))
+b = complex(5, 9)
+print(b)
+print("The type of b is ", type(b))
+c = "Harry"
+print(c)
+print("The type of c is ", type(c))
