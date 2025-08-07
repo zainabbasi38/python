275f13c9afa4aa01b993ddfe8aa07477bfd4d551
zainabbasi38 <zainabbasi7534@gmail.com>
2025-08-07 18:51:39 +0500

    strings

---
+++ day-11/main.py
@@ -0,0 +1,14 @@
+first_name = "Zain"
+second_name = " Abbasi"
+print(first_name+ second_name)
+greetings = """
+Hi
+how are you?
+hello
+"""
+
+print(first_name[1])
+print(greetings)
+
+for character in greetings:
+    print(character)
