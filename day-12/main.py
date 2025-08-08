83dd4fc321b0b8bfcd74fbfe13224fd4601c2989
zainabbasi38 <zainabbasi7534@gmail.com>
2025-08-08 21:39:31 +0500

    strings

---
+++ day-12/main.py
@@ -0,0 +1,13 @@
+names = "Zain,saad"
+# print(names[0:4])
+print(len(names))
+print(names[2:3])
+
+#Negative slicing
+print(names[-4:6])
+            # 5:6
+
+
+# Quiz
+nm = "Zain"
+print(nm[-4:-2])
