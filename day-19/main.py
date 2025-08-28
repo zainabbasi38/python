23e566a4c39fa397b5f37fee01032c04481c0cab
zainabbasi38 <zainabbasi7534@gmail.com>
2025-08-28 19:48:40 +0500

    break-&-continue

---
+++ day-19/main.py
@@ -0,0 +1,12 @@
+# for i in range(12):
+#     if i == 10:
+#         # break
+#         print("Skipping 10")
+#         continue
+#     print("5 * ",i, "=", 5*i)
+i = 0
+while True:
+    print(i)
+    i = i+1
+    if i % 100 == 0:
+        break
