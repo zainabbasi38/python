d2fc2a9eb5420ebe4b43f31272eccaa50e99cba6
zainabbasi38 <zainabbasi7534@gmail.com>
2025-09-02 15:53:29 +0500

    list-methods

---
+++ day-23/main.py
@@ -0,0 +1,18 @@
+lst = [2,3,4,5,2,0,6,2,9,4,5]
+print(lst)
+# lst.append(8)
+# lst.sort()
+# lst.sort(reverse=True)
+# print(lst.count(9))
+# m = lst.copy()
+# m[0] = 33
+# print(m)
+
+# lst.insert(2, 322)
+# print(lst)
+
+m = [200, 400, 500]
+# lst.extend(m)
+k = lst + m
+print(k)
+print(lst)
