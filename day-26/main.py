fcb7515716ac23d9ce9d65867a67b14f39fb8a7a
zainabbasi38 <zainabbasi7534@gmail.com>
2025-09-05 19:19:03 +0500

    solution

--- day-26/main.py
+++ day-26/main.py
@@ -4,10 +4,15 @@ timestamp = time.strftime("%H:%M:%S")
 hours = int(time.strftime("%H"))
 # hours = 15
 if hours >= 0 and hours < 12:
+    
     print("Good Morning")
-elif hours >= 12 and hours < 16:
+elif hours >= 12 and hours <= 17:
+    if hours > 12:
+        hours = hours - 12
     print("Good AfterNoon")
 else:
+    
+    hours = hours - 12
     print("Good Night")
 
-print(timestamp)
+print("It's time is",hours,"o'clock")
