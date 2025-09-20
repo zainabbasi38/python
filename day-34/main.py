3344961c09bba39befe6c4bb1ef130c0eed6851f
zainabbasi38 <zainabbasi7534@gmail.com>
2025-09-20 21:17:42 +0500

    dict-methods

---
+++ day-34/main.py
@@ -0,0 +1,10 @@
+ep1 = {333:79, 212:64, 123:45, 456:89}
+ep2 = {221:70, 892:69}
+# ep1.update(ep2)
+# ep1 |= ep2
+# ep1.clear()
+# ep1.pop(123)
+# ep1.popitem()
+# del ep1, ep2
+del ep1[212]
+print(ep1)
